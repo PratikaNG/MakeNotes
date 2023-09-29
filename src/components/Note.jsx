847a7473e62a326ef93674c8ca8f0885207ab3bc
Pratika NG <pratika.05ng@gmail.com>
2023-09-29 13:39:10 +0530

    mapping notes

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,8 +1,15 @@
+import notes from "../notes"
 const Note = () => {
   return (
-    <div className="note-card">
-      <h1 className="note-title">title</h1>
-      <p className="note-text">this is note text</p>
+    <div className="notes">
+      {notes.map((note) => (
+        <div className="note-card" key={note.id}>
+          <h1 className="note-title">{note.jokeType}</h1>
+          <p className="note-text">{note.jokeText}</p>
+          
+        </div>
+      ))}
+     
     </div>
   );
 };
---
+++ src/notes.js
@@ -0,0 +1,60 @@
+const notes = [
+    {
+    id: 1,
+    jokeText:
+      "Why don't scientists trust atoms? Because they make up everything.",
+    jokeType: "Science",
+  },
+  {
+    id: 2,
+    jokeText:
+      "Why did the scarecrow win an award? Because he was outstanding in his field.",
+    jokeType: "Puns",
+  },
+  {
+    id: 3,
+    jokeText:
+      "I told my wife she was drawing her eyebrows too high. She looked surprised.",
+    jokeType: "Puns",
+  },
+  {
+    id: 4,
+    jokeText:
+      "What did one ocean say to the other ocean? Nothing, they just waved.",
+    jokeType: "Wordplay",
+  },
+  {
+    id: 5,
+    jokeText:
+      "Why do we never tell secrets on a farm? Because the potatoes have eyes and the corn has ears.",
+    jokeType: "Wordplay",
+  },
+  {
+    id: 6,
+    jokeText: "How do you organize a space party? You planet!",
+    jokeType: "Science",
+  },
+  {
+    id: 7,
+    jokeText:
+      "Why don't some couples go to the gym? Because some relationships don't work out.",
+    jokeType: "Puns",
+  },
+  {
+    id: 8,
+    jokeText:
+      "Parallel lines have so much in common. It's a shame they'll never meet.",
+    jokeType: "Math",
+  },
+  {
+    id: 9,
+    jokeText: "What do you call fake spaghetti? An impasta!",
+    jokeType: "Food",
+  },
+  {
+    id: 10,
+    jokeText: "Why did the tomato turn red? Because it saw the salad dressing!",
+    jokeType: "Food",
+  }]
+
+  export default notes
